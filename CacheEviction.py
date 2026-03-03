97b27546d3f555f80c6b8ed2b4214b154b29312c
Nathan King <nathanking@Nathans-MacBook-Air.local>
2026-03-03 10:43:33 -0500

    changed class structure

---
+++ CacheEviction.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class FIFO:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.misses = 0
+
+class LRU:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.misses = 0
+
+class OPTFF:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.misses = 0
+
+
+
+
+    
