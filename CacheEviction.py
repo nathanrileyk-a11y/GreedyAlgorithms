9f6d3ad467bf997234dd985761a8ea9370e69fca
Nathan King <nathanking@Nathans-MacBook-Air.local>
2026-03-03 10:57:36 -0500

    preprocessing

--- CacheEviction.py
+++ CacheEviction.py
@@ -1,9 +1,15 @@
 from collections import deque
 
+
+def load_data(file_path):
+    with open(file_path, 'r') as file:
+        x = file.readline()
+        print(x)
 class FIFO:
     def __init__(self, capacity):
         self.capacity = capacity
         self.misses = 0
+
 
 class LRU:
     def __init__(self, capacity):
@@ -19,3 +25,5 @@ class OPTFF:
 
 
     
+print(load_data("data.txt"))
+
